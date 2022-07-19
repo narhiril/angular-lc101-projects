9ac190f2c93393d6e8c49d9e553ddd98aea99982
narhiril <roxannarusbarsky@yahoo.com>
2022-07-19 18:33:05 -0500

    assignment 1

---
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fav-links',
+  templateUrl: './fav-links.component.html',
+  styleUrls: ['./fav-links.component.css']
+})
+export class FavLinksComponent implements OnInit {
+
+  favLinks: string[] = [
+    'https://www.stlouisrocketry.com/',
+    'https://www.nar.org/'
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Pictures of Silkmoths';
+  image1 = 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F24%2F2019%2F07%2Fgettyimages-128549064-2000.jpg';
+  image2 = 'https://i.pinimg.com/originals/d9/56/ac/d956ac62028d4e9c5190d89ae5b6c36f.jpg';
+  image3 = 'https://i.pinimg.com/736x/2e/41/89/2e418952359305053b8df0495b18f9d3.jpg';
 
   constructor() { }
 
